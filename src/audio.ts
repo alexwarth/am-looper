9a77f2b44ba296705112b8c9b5b9c1264ebc118e
Alessandro Warth <alexwarth@gmail.com>
2025-04-11 16:24:20 -0700

    add UI for input device options (auto gain control, echo cancellation)

--- src/audio.ts
+++ src/audio.ts
@@ -1,16 +1,15 @@
 import { InputDeviceInfo } from './types';
 
+const options = { autoGainControl: false, echoCancellation: false };
+
 export async function init(
   context: AudioContext,
   looper: AudioWorkletNode,
 ): Promise<InputDeviceInfo> {
   const deviceId = await selectDevice();
+  console.log(options);
   const micStream = await navigator.mediaDevices.getUserMedia({
-    audio: {
-      deviceId,
-      autoGainControl: false,
-      echoCancellation: false, // TODO: try it, see how it affects latency?
-    },
+    audio: { deviceId, ...options },
   });
   const mic = context.createMediaStreamSource(micStream);
 
@@ -30,7 +29,7 @@ async function selectDevice() {
   const devices = await navigator.mediaDevices.enumerateDevices();
   return new Promise<string>((resolve) => {
     const buttons = document.getElementById('buttons') as HTMLDivElement;
-    buttons.appendChild(div(document.createTextNode('Select input device:')));
+    buttons.appendChild(tag('div', document.createTextNode('Select input device:')));
 
     for (const d of devices) {
       if (d.kind !== 'audioinput') {
@@ -45,13 +44,34 @@ async function selectDevice() {
       };
       buttons.appendChild(button);
     }
+
+    buttons.appendChild(tag('div', document.createTextNode('')));
+    buttons.appendChild(tag('div', document.createTextNode('Options:')));
+    buttons.appendChild(option('autoGainControl', 'auto gain control'));
+    buttons.appendChild(option('echoCancellation', 'echo cancellation'));
   });
 }
 
-function div(...children: any) {
-  const el = document.createElement('div');
+function tag(name: string, ...children: any) {
+  const el = document.createElement(name);
   for (const child of children) {
     el.appendChild(child);
   }
   return el;
+}
+
+function option(name: string, label: string) {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = options[name];
+  checkbox.id = name;
+  checkbox.onchange = () => {
+    options[name] = checkbox.checked;
+  };
+  return tag('span', checkbox, document.createTextNode(' ' + label));
+}
+
+function getOptionValue(id: string, defaultValue = false): boolean {
+  console.log(id, '=', document.getElementById(id));
+  return (document.getElementById(id) as HTMLInputElement)?.checked ?? defaultValue;
 }
